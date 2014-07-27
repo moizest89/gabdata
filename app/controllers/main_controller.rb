628cd6b2b59bed5f5fc5142c9a9bf653bf040eb5
Pablo Marti <pablo.marti89@gmail.com>
2014-07-27 16:51:37 -0600

    fix conflict

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,36 +7,6 @@ class MainController < ApplicationController
 
   def search_result
   	#render :layout => "search_result"
-    
-  	@keywords_select = Keyword.all.to_a.map{|k| [k.label_for_select, k.keyword]}
-    @results = []
-  	@error_message = ""
-  	@response = ""
-  	@parsed_response = ""
-  	@url = ""
-  	@search_value = ""
-  	if params[:strato]
-  		@search_value = params[:strato][:phrase]
-      search_param = @search_value.to_s.split(" ").join("+")
-	  	begin
-			  @url = "http://107.170.49.159:8983/solr/collection1/select?q=#{search_param}&wt=json&indent=true&defType=edismax&stopwords=true&lowercaseOperators=true"
-			  url_parsed = URI.parse(@url)
-			  @response = Net::HTTP.get_response(url_parsed)
-			  @parsed_response = JSON.parse(@response.body)
-			  #@total_results = @parsed_response["response"]["numFound"]
-			  @results = @parsed_response["response"]["docs"]
-			rescue => error
-				@error_message = "Your search didn't get any result"
-			end
-      
-			render "search_result", :layout => "search_result"
-		else
-			render :layout => "search"
-	  end
-  end
-
-  def search_result
-  	render :layout => "search_result"
 
   	@results = []
   	@error_message = ""
@@ -45,6 +15,7 @@ class MainController < ApplicationController
   	@url = ""
   	@search_value = ""
   	@keyword = Keyword.where(keyword: params[:keyword]).first
+    @keywords_select = Keyword.all.to_a.map{|k| [k.label_for_select, k.keyword]}
   	@models = @keyword.get_models_classes
   	
   	logger.debug "START"
@@ -62,8 +33,11 @@ class MainController < ApplicationController
 			  @parsed_response = JSON.parse(@response.body)
 			  #@total_results = @parsed_response["response"]["numFound"]
 			  @results = @parsed_response["response"]["docs"]
+        render "search_result", :layout => "search_result"
 			rescue => error
 				@error_message = "Your search didn't get any result"
+
+        render :layout => "search"
 			end
   	end
   end
