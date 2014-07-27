75b4a092fa4edb04d020dd3ecca6372779b9a368
moizest89 <moises.gilberto@gmail.com>
2014-07-27 02:13:18 -0600

    devise footer and search result

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'main/index'
 
+  get 'main/search_result'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
